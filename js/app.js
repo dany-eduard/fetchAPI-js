2658fae0fa679924e073a9e3bb3cc5903bf2853c
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-07-18 00:15:22 -0500

    datos.txt

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 document.getElementById("txtBtn").addEventListener("click", cargarTXT);
 
 function cargarTXT() {
-  fetch("datos.txt").then(function (res) {
-    console.log(res);
-  });
+  //Busca datos.txt (URL), y lo devuelve en res
+  fetch("datos.txt")
+    .then(function (res) {
+      return res.text(); //Usar método .text() para mostrarlo
+    })
+    .then(function (data) {
+      console.log(data);
+      document.getElementById("resultado").innerHTML = data;
+    })
+    .catch(function (error) {
+      alert(error);
+      console.log(error);
+    });
+  /**
+   * Usualmente se requieren 2 then. El primero para conectarse y
+   * devolver los datos, en donde se le pasa el método de acuerdo al
+   * tipo de dato y, el segundo then que muestra los datos.
+   * El catch se usa para mostrar errores en caso de que ocurran.
+   */
 }

86512cad3e89d553ea1a280cda707a90f6871ddc
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-07-18 00:59:43 -0500

    Cargar un JSON y cargar una API

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 document.getElementById("txtBtn").addEventListener("click", cargarTXT);
+document.getElementById("jsonBtn").addEventListener("click", cargarJSON);
+document.getElementById("apiBTN").addEventListener("click", cargarAPI);
 
 function cargarTXT() {
   //Busca datos.txt (URL), y lo devuelve en res
@@ -11,7 +13,6 @@ function cargarTXT() {
       document.getElementById("resultado").innerHTML = data;
     })
     .catch(function (error) {
-      alert(error);
       console.log(error);
     });
   /**
@@ -20,4 +21,42 @@ function cargarTXT() {
    * tipo de dato y, el segundo then que muestra los datos.
    * El catch se usa para mostrar errores en caso de que ocurran.
    */
+}
+
+function cargarJSON() {
+  fetch("empleados.json")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let html = "";
+      data.forEach(function (item) {
+        html += `
+          <li>${item.nombre} - ${item.puesto}</li>
+        `;
+      });
+      document.getElementById("resultado").innerHTML = html;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function cargarAPI() {
+  fetch("https://picsum.photos/list")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let html = `<h1>picsum.photos</h1>`;
+      data.forEach(function (item) {
+        html += `
+          <li>${item.author} - <a href="${item.post_url}">Ver imagen</a> </li>
+        `;
+      });
+      document.getElementById("resultado").innerHTML = html;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
